5e5a09a534acc9c577bef8ead54c858bc6b439e8
laura fernando <laura.fernando@gmail.com>
2012-04-17 13:56:24 +0200

    cambios

--- gen/com/curso/android/R.java
+++ gen/com/curso/android/R.java
@@ -21,27 +21,42 @@ public final class R {
     }
     public static final class id {
         public static final int bot1=0x7f060004;
+        public static final int input1=0x7f060006;
+        public static final int input2=0x7f060008;
+        public static final int input3=0x7f06000a;
         public static final int text1=0x7f060000;
         public static final int text2=0x7f060001;
         public static final int text3=0x7f060002;
         public static final int text4=0x7f060003;
-        public static final int text5=0x7f060005;
-        public static final int text6=0x7f060006;
+        public static final int text5=0x7f06000b;
+        public static final int text6=0x7f06000c;
+        public static final int texto1=0x7f060005;
+        public static final int texto2=0x7f060007;
+        public static final int texto3=0x7f060009;
     }
     public static final class layout {
         public static final int atributos=0x7f030000;
         public static final int boton=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int nuevo=0x7f030003;
-        public static final int relativelay=0x7f030004;
+        public static final int ejemplotextfield=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int nuevo=0x7f030004;
+        public static final int relativelay=0x7f030005;
     }
     public static final class string {
+        public static final int apellidos=0x7f05000d;
         public static final int app_name=0x7f050001;
         public static final int autodes=0x7f050006;
+        public static final int bien=0x7f050007;
+        public static final int borrar=0x7f05000a;
+        public static final int cancelar=0x7f050009;
+        public static final int ciudad=0x7f05000e;
+        public static final int enviar=0x7f050008;
         public static final int hello=0x7f050000;
+        public static final int nombre=0x7f05000c;
         public static final int primero=0x7f050002;
         public static final int segundo=0x7f050003;
         public static final int tercero=0x7f050004;
+        public static final int titulo=0x7f05000b;
         public static final int ultimo=0x7f050005;
     }
 }
--- src/com/curso/android/CursoAndroidLauraActivity.java
+++ src/com/curso/android/CursoAndroidLauraActivity.java
@@ -1,18 +1,37 @@
 package com.curso.android;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class CursoAndroidLauraActivity extends Activity {
     /** Called when the activity is first created. */
-    @Override
+    TextView texto;
+    
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        texto =(TextView)findViewById(R.id.text1);
+        getResources().getColor(R.color.blanco);
     }
     public void click(View view) {
+    	texto.setText("esta aplicación se autodestruirá");
     	Log.e("AUTODES", "Esta aplicación se autodestruirá...");
+    	//findViewById(R.id.text1)
+    	LinearLayout linLay=(LinearLayout)texto.getParent();
+    	linLay.setBackgroundColor(Color.BLUE);
+    }
+    public void clickEnviar(View view) {
+    	texto=(TextView)findViewById(R.id.text1);
+    	TextView msg=(TextView) findViewById(R.id.text2);
+    	msg.setText(texto.getText().toString());
+    }
+    public void clickBorrar(View view) {
+    	TextView msg=(TextView)findViewById(R.id.text1);
+
     }
 }
