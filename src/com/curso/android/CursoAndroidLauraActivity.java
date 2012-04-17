cadc47f9f9faa145081c080e77503ea6298cff7e
laura fernando <laura.fernando@gmail.com>
2012-04-17 12:53:51 +0200

    cambios hasta boton

--- src/com/curso/android/CursoAndroidLauraActivity.java
+++ src/com/curso/android/CursoAndroidLauraActivity.java
@@ -2,6 +2,8 @@ package com.curso.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class CursoAndroidLauraActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,8 @@ public class CursoAndroidLauraActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    public void click(View view) {
+    	Log.e("AUTODES", "Esta aplicación se autodestruirá...");
     }
 }
